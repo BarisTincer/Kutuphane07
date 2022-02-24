6017752f284c4e3530acac98c8b48e7c0eddff1a
BARIŞ TİNCER <baris.tincer@gmail.com>
2022-02-24 11:33:02 +0300

    Add project files.

---
+++ Kutuphane07.DATA/Kullanici.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane07.DATA
+{
+    public class Kullanici
+    {
+        public Kullanici()
+        {
+            Id = Guid.NewGuid();
+            OduncuAlinanKitaplar = new List<Kitap>();
+        }
+        public Guid Id { get; set; }
+        public string AdSoyad { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Parola { get; set; }
+        public List<Kitap> OduncuAlinanKitaplar { get; set; }
+    }
+}
---
+++ Kutuphane07.DATA/KullaniciYoneticisi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane07.DATA
+{
+    public class KullaniciYoneticisi
+    {
+        public KullaniciYoneticisi()
+        {
+            Kullanicilar = new List<Kullanici>();
+        }
+        public List<Kullanici> Kullanicilar { get; set; }
+        public void KayitOlma(string adSoyad, string kullaniciAdi, string parola)
+        {
+            Kullanici kullanici = new Kullanici()
+            {
+                AdSoyad = adSoyad,
+                Parola = parola,
+                KullaniciAdi = kullaniciAdi
+            };
+            Kullanicilar.Add(kullanici);
+        }
+        public Kullanici GirisYap(string kullaniciAdi, string parola)
+        {
+            return Kullanicilar.FirstOrDefault(x => x.KullaniciAdi == kullaniciAdi && x.Parola == parola);
+        }
+        public bool KullaniciVarmi(string kullaniciAdi)
+        {
+            return Kullanicilar.Any(x => x.KullaniciAdi == kullaniciAdi);
+        }
+    }
+}
